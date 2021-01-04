0fa3098db74a34557ea63b9d478d28cfc0735678
Aaron-cdx <1315020626@qq.com>
2021-01-04 09:53:32 +0800

    Initial commit

---
+++ src/main/java/com/duanxi/aaron/AaronBlogApplication.java
@@ -0,0 +1,13 @@
+package com.duanxi.aaron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaronBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AaronBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duanxi/aaron/AaronBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duanxi.aaron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaronBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
